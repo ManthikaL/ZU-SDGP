beba967fbb91475b1d53463535d0653f67f83ff3
Tharindu Darshana <94457266+tharindudpathiraja@users.noreply.github.com>
2022-05-15 22:23:10 +0530

    Add files via upload

---
+++ oneplus-nord2/src/components/navbar/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Nav.css"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <div className="logo">ASK ME <b>Phone</b></div>
+            <ul>
+                <li>Home</li>
+                <li>Ratings & Reviews</li>
+                <li>About Us</li>
+            </ul>        
+        </nav>
+
+    )
+}
